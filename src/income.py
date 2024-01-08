95fd6b70fc918190dc643a6ba0bc072d45d6a4c0
jfreun123 <jfreun1@gmail.com>
2024-01-07 20:23:41 -0500

    tweaks

--- src/income.py
+++ src/income.py
@@ -5,12 +5,10 @@ from IncomeModel.random_walk_simulation import Simulation
 
 sim = Simulation(end_age=105, 
                  curr_age=23,
-                 retirement_age=70, 
+                 retirement_age=60, 
                  starting_amount=30_000, 
                  inflation=1.03)
 plotter = Plotter(starting_age = 23)
-# points = sim.single_simulation()
-# plotter.basic_plot(points=points)
 
-n = 1
+n = 10
 plotter.plot_all(sim.many_simulations(number_of_simulations=n), number_of_simulations=n)
--- src/salary.t.py
+++ src/salary.t.py
@@ -1,4 +1,4 @@
 from SalaryCalculations.salary import Salary
 
-test = Salary(salary=500_000, roth_deductions=16080)
+test = Salary(salary=280_000,post_tax_semi_monthly=6866*.93 ,roth_deductions=16080)
 print(test)
